13166d9fb06492428165daa6abc0b33ed1dcb4fb
suyash <suyash.k@chapter247.com>
2022-04-25 20:06:06 +0530

    implement  hbs for line graph

--- src/app.js
+++ src/app.js
@@ -6,6 +6,11 @@ import { fileURLToPath } from "url";
 import router from "./api/routes/v1/index.js";
 import passport from "passport";
 import { googleLogin } from "./api/config/passport.js";
+import { engine } from "express-handlebars";
+import { allowInsecurePrototypeAccess } from "@handlebars/allow-prototype-access";
+import Handlebars from "handlebars";
+import path from "path";
+
 googleLogin(passport);
 
 dotenv.config(); // Here is env file configuration
@@ -20,6 +25,22 @@ app.use(express.urlencoded({ extended: true })); // use for getting form url dat
 
 app.use(router);
 
+// handlebars code start here
+app.engine(
+  ".hbs",
+  engine({
+    extname: ".hbs",
+    defaultLayout: "main",
+    handlebars: allowInsecurePrototypeAccess(Handlebars),
+  })
+);
+app.set("view engine", ".hbs"); //Sets our app to use the handlebars engine
+
+app.set("views", path.join(__dirname, "views")); //Sets handlebars configurations
+
+app.get("/line/graph",async(req,res)=>{
+  return res.render("home/home")
+})
 // create server code start here
 const port = process.env.PORT;
 
